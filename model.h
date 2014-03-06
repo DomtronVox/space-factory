da060f576d88550e901b8c48caf7b67c01792e99
splemp <splemp@gmail.com>
2014-03-05 22:52:24 -0500

    Created file model.h and put current (incomplete) design pattern into that file correctly formatted

---
+++ model.h
@@ -0,0 +1,70 @@
+#include <QTimer>
+
+#ifndef MODEL_H
+#define MODEL_H
+
+Class Map;
+
+class Model {
+ //singleton design
+ //variables
+    int * highscores;
+    int current;
+    Map* map;
+};
+
+//functions
+class HighScore {
+//variables
+    string filename; //should we have this be a file object?
+    vector<string> names;
+    vector<int> scores;
+
+//functions
+    constructor(filename);
+    destructor();
+    save();
+};
+
+class Map {
+//variables
+    static id;
+    vector<PosEntity> entities;
+
+//functions
+    create entity(owner, type, pos);
+    kill entity(id);
+    update();
+};
+
+class Angular {
+//variables
+    int angle;
+
+//functions
+    turnToPoint(x,y);
+    checkAngle();
+};
+
+
+class Health {
+//variables
+    int maxHealth;
+    int curHealth;
+
+//functions
+    hit();
+    die();
+};
+
+
+class Weapon {
+//variables
+    QTimer cooldown;
+    int damage;
+
+//functions
+
+};
+
+#endif // MODEL_H
