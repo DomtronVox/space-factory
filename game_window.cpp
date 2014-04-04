4dcd1a72a48508adb4569c84ba534415533b5f10
splemp@gmail.com <splemp@gmail.com>
2014-04-04 10:20:14 -0400

    fixed score display bug

--- game_window.cpp
+++ game_window.cpp
@@ -50,7 +50,7 @@ GameWindow::~GameWindow()
 
 void GameWindow::timerHit()
 {
-    //ui->scr->setText(QString::number(Model::instance()->getScr));
+    ui->scr->setText(QString::number(Model::instance()->getScr()));
 
     //loop through models new entity collection
     for (BaseEntity *e : Model::instance()->getRecentlyCreated()){
